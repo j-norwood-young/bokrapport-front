b1fcea4e3a654f0fae67e13172a09d34ded90074
Jason Norwood-Young <jason@10layer.com>
2015-09-05 15:29:21 +0200

    Reset slider on resize

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,23 +2,35 @@ var jquery = require("jquery");
 
 $(function() {
 	
-	$(".slider").each(function() {
-		// console.log(this);
-		var top = $(this).css("margin-top");
-		var left = $(this).position().left;
-		var maxx = $(this).parent().width() - $(this).width();
-		var id = $(this).data("id");
-		console.log(left, maxx);
-		new Draggable(this, {
-			limit: {
-				y: top,
-				x: [left, maxx  + left]
-			},
-			onDrag: function(el, x) {
-				console.log("x", x, "left", left, "maxx", maxx, "result", x/maxx);
-				$("#user_rating_" + id).html(Math.round((x - left) / maxx * 10));
-			}
+	var draggables = [];
+
+	var makeSliders = function() {
+		console.log("makeSliders");
+		draggables = [];
+		$(".slider").each(function() {
+			// console.log(this);
+			$(this).css("left", $(this).parent().position().left);
+			var top = $(this).css("margin-top");
+			var left = $(this).position().left;
+			var maxx = $(this).parent().width() - $(this).width();
+			var id = $(this).data("id");
+			// console.log(left, maxx);
+			var draggable = new Draggable(this, {
+				limit: {
+					y: top,
+					x: [left, maxx  + left]
+				},
+				onDrag: function(el, x) {
+					// console.log("x", x, "left", left, "maxx", maxx, "result", x/maxx);
+					$("#user_rating_" + id).html(Math.round((x - left) / maxx * 10));
+				}
+			});
+			draggables.push(draggable);
 		});
-	})
+	}
+
+	$(window).on("resize", makeSliders);
+
+	makeSliders();
 	
 })
