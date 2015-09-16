fba3fc65d7b69416064851fbffb0494a67d0b398
Jason Norwood-Young <jason@10layer.com>
2015-09-16 07:41:42 +0200

    Google Analytics

--- assets/js/index.js
+++ assets/js/index.js
@@ -137,7 +137,15 @@ $(function() {
 	}
 });
 
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-18329512-3', 'auto');
+ga('send', 'pageview');
 
+// Facebook
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
